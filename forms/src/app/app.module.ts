c6f1d4f1470478715bda10e6116eae3dfb46efcc
Weslei Ferreira <weslei.fs@hotmail.com>
2017-08-14 22:09:42 -0300

    Aulas de Form

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-template-form',
@@ -12,15 +14,94 @@ export class TemplateFormComponent implements OnInit {
     email: null
   }
 
-  constructor() { }
+  //Para se utilizar o Http, é necessário importar o módulo HttpModule do @angular/http
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
-  onSubmit(form){
+  onSubmit(form) {
     console.log(form);
-    console.log("Usuário:",this.usuario);
+    //console.log(this.usuario);
 
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+    .map(res => res)
+    .subscribe(dados => console.log(dados)
+    )
+
+
+  }
+
+  verificaValidTouched(campo) {
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'has-herror': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCEP(cep: any, form: any) {
+    //Remove todos os caracteres diferentes de números
+    cep = cep.replace(/\D/g, '');
+    this.resetaDadosForm(form);
+    if (cep !=""){
+      //Expressão regular
+      var validaCEP = /^[0-9]{8}$/;
+      if (validaCEP.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .map(dados => dados.json())
+          .subscribe(dados => this.populaDadosForm(dados, form))
+      }
+    }
+
+  }
+
+  populaDadosForm(dados: any, formulario: any) {
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     rua: dados.logradouro,
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf,
+
+    //   }
+    // })
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+
+    })
+
+    //console.log(formulario);
+
+  }
+
+  resetaDadosForm(formulario: any){
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        cep: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
 
   }
 
