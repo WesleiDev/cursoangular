a5d5313bcb085dc8d4a9cee42c17e14c6a52b3f0
Weslei Ferreira <weslei.fs@hotmail.com>
2017-08-22 22:50:10 -0300

    22/08/2017

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-data-form',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataFormComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: Http ) { }
 
   ngOnInit() {
+
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl(null),
+    //   email : new FormControl(null)
+    // });
+
+    //Criando um formulário do tipo DataForm
+    this.formulario = this.formBuilder.group({
+      nome:[null],
+      email:[null]
+    })
+
   }
+
+  onSubmit(){
+    console.log(this.formulario.value);
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+    .map(res => res)
+    .subscribe(dados => console.log(dados)
+    )
+
+    }
 
 }
